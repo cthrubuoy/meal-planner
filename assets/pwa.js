4f0540797f1df02daa3b60072bcdc42744acd05b
cthrubuoy <christreharne@gmail.com>
2026-02-01 16:28:53 +0000

    Update pwa.js

---
+++ assets/pwa.js
@@ -0,0 +1,6 @@
+// Service worker registration (PWA)
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./sw.js").catch(() => {});
+  });
+}
