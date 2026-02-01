ccc98737b2522b5dcbeba871170a9749b2f30ccb
cthrubuoy <christreharne@gmail.com>
2026-02-01 17:10:23 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "meal-planner-v9-1";
+const CACHE_NAME = "meal-planner-v9-2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -49,3 +49,4 @@ self.addEventListener("fetch", (event) => {
 
   event.respondWith(caches.match(req).then((cached) => cached || fetch(req)));
 });
+
